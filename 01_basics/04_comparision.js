f6707b0d5e0dd5c3da967175b8bac97267638ffa
vRahul <rahulverma2305@yahoo.com>
2023-12-22 19:42:13 +0000

    data comparision tested in this session

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 === 2)
+
+// console.log("2" > 1)
+// console.log(2 > "1")
+// console.log("2" < 1)
+// console.log(2 < "1")
+// console.log("2" == 2)
+// console.log("2" === 2) // === is for strict comparision. ie.e data type and value both needs to be same.
+
+// null converts differently with comparision operators.
+// null converts to number for comparision operators except ==.
+// null coverts to NaN with == opertor.
+// console.log(null > 0) 
+// console.log(null <= 0)
+// console.log(null == 0)
+
+
+console.log(undefined > 0)
+console.log(undefined <= 0)
+console.log(undefined == 0)
+console.log(typeof (undefined > 0)) //boolean
