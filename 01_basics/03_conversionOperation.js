2ca6e5b65a23c3c88347b9c925bef6d5a071495b
vRahul <rahulverma2305@yahoo.com>
2023-12-22 18:54:50 +0000

    updated with operation conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,24 +2,24 @@ let score = "33abc"
 
 let scoreInNumber = Number(score)
 
-console.log(typeof score);
-console.log(score);
-console.log(typeof scoreInNumber);
-console.log(scoreInNumber);
+// console.log(typeof score);
+// console.log(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
 
 
 let isNumber = false
 let boolInNum = Number(isNumber)
-console.log(typeof isNumber);
-console.log(typeof boolInNum);
-console.log(boolInNum);
+// console.log(typeof isNumber);
+// console.log(typeof boolInNum);
+// console.log(boolInNum);
 
 
 let myName = ""
 let isName = Boolean(myName)
 
-console.log(typeof isName);
-console.log(isName);
+// console.log(typeof isName);
+// console.log(isName);
 
 
 // "33" => 33
@@ -29,5 +29,34 @@ console.log(isName);
 let someNum = 1234
 let numInString = String(someNum)
 
-console.log(numInString);
-console.log(typeof numInString);
+// console.log(numInString);
+// console.log(typeof numInString);
+
+
+
+// ***************** Operation *********************
+// console.log(4+4);
+// console.log(4-2);
+// console.log(4*3);
+// console.log(3/4);
+// console.log(3%4);
+// console.log(5%4);
+// console.log(3**4);
+
+let str1 = "Hello"
+let str2 = " Rahul"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 5);
+// console.log(1 + "5");
+// console.log(1 + 5 + "7");
+// console.log("1" + 5 + 7);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+// console.log(+"Rahul");
+// console.log(+"33");
